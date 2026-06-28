09c5525cfb502830c2eab3d6a6f4d1f24a859e27
Subrahmanya CP <subrahmanyasubrahmanya1389@gmail.com>
2026-06-29 01:28:07 +0530

    debug job routes

--- backend/routes/jobMatchRoutes.js
+++ backend/routes/jobMatchRoutes.js
@@ -1,5 +1,15 @@
+// import express from 'express';
+// import { matchJob } from '../controllers/jobMatchController.js';
+
+// const router = express.Router();
+
+// router.post('/match', matchJob);
+
+// export default router;
 import express from 'express';
 import { matchJob } from '../controllers/jobMatchController.js';
+
+console.log("✅ jobMatchRoutes loaded");
 
 const router = express.Router();
 
